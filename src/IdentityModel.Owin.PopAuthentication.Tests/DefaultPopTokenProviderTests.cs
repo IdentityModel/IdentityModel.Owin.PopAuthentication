c1fed91811ec18f2b8bd6485eccf3617942d8b51
Brock Allen <brockallen@gmail.com>
2016-02-28 11:01:07 -0500

    build token provider to locate pop token

---
+++ src/IdentityModel.Owin.PopAuthentication.Tests/DefaultPopTokenProviderTests.cs
@@ -0,0 +1,145 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+using Microsoft.Owin;
+using System.IO;
+
+namespace IdentityModel.Owin.PopAuthentication.Tests
+{
+    public class DefaultPopTokenProviderTests
+    {
+        [Fact]
+        public async Task when_no_params_GetPopTokenAsync_should_return_null()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Method = "GET";
+            ctx.Request.Path = new PathString("/hello");
+            var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+            token.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task when_POST_body_has_no_token_GetPopTokenAsync_should_return_null()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Method = "POST";
+            ctx.Request.Path = new PathString("/hello");
+            ctx.Request.ContentType = "application/x-www-form-urlencoded";
+            using (var ms = new MemoryStream())
+            {
+                var form = "foo=bar&baz=quux";
+                var bytes = Encoding.UTF8.GetBytes(form);
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Seek(0, SeekOrigin.Begin);
+                ctx.Request.Body = ms;
+
+                var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+                token.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public async Task when_query_has_no_token_GetPopTokenAsync_should_return_null()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Method = "GET";
+            ctx.Request.Path = new PathString("/hello");
+            ctx.Request.QueryString = new QueryString("x=1&y=2");
+
+            var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+            token.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetPopTokenAsync_should_find_token_in_authorization_header()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Headers.Add("Authorization", new string[] { "PoP token" });
+
+            var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+            token.Should().Be("token");
+        }
+
+        [Fact]
+        public async Task GetPopTokenAsync_should_find_token_in_POST_body()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Method = "POST";
+            ctx.Request.Path = new PathString("/hello");
+            ctx.Request.ContentType = "application/x-www-form-urlencoded";
+            using (var ms = new MemoryStream())
+            {
+                var form = "foo=bar&pop_access_token=token&baz=quux";
+                var bytes = Encoding.UTF8.GetBytes(form);
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Seek(0, SeekOrigin.Begin);
+                ctx.Request.Body = ms;
+
+                var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+                token.Should().Be("token");
+            }
+        }
+
+        [Fact]
+        public async Task GetPopTokenAsync_should_find_token_in_query_string()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Path = new PathString("/hello");
+            ctx.Request.QueryString = new QueryString("x=1&pop_access_token=token&y=2");
+
+            var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+            token.Should().Be("token");
+        }
+
+        [Fact]
+        public async Task when_multiple_tokens_sent_GetPopTokenAsync_should_find_authorization_header_first()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Method = "POST";
+            ctx.Request.Path = new PathString("/hello");
+            ctx.Request.Headers.Add("Authorization", new string[] { "PoP token1" });
+            ctx.Request.QueryString = new QueryString("x=1&pop_access_token=token3&y=2");
+            ctx.Request.ContentType = "application/x-www-form-urlencoded";
+            using (var ms = new MemoryStream())
+            {
+                var form = "foo=bar&pop_access_token=token2&baz=quux";
+                var bytes = Encoding.UTF8.GetBytes(form);
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Seek(0, SeekOrigin.Begin);
+                ctx.Request.Body = ms;
+
+                var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+                token.Should().Be("token1");
+            }
+        }
+
+        [Fact]
+        public async Task when_multiple_tokens_sent_GetPopTokenAsync_should_find_POST_body_second()
+        {
+            var ctx = new OwinContext();
+            ctx.Request.Method = "POST";
+            ctx.Request.Path = new PathString("/hello");
+            ctx.Request.QueryString = new QueryString("x=1&pop_access_token=token3&y=2");
+            ctx.Request.ContentType = "application/x-www-form-urlencoded";
+            using (var ms = new MemoryStream())
+            {
+                var form = "foo=bar&pop_access_token=token2&baz=quux";
+                var bytes = Encoding.UTF8.GetBytes(form);
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Seek(0, SeekOrigin.Begin);
+                ctx.Request.Body = ms;
+
+                var token = await DefaultPopTokenProvider.GetPopTokenAsync(ctx.Environment);
+                token.Should().Be("token2");
+            }
+        }
+    }
+}
---
+++ src/IdentityModel.Owin.PopAuthentication/DefaultPopTokenProvider.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel.HttpSigning;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityModel.Owin.PopAuthentication
+{
+    public class DefaultPopTokenProvider
+    {
+        public static async Task<string> GetPopTokenAsync(IDictionary<string, object> env)
+        {
+            if (env == null) throw new ArgumentNullException("env");
+
+            var ctx = new OwinContext(env);
+            
+            if (ctx.Request.Headers.ContainsKey("Authorization"))
+            {
+                var authorizationHeader = ctx.Request.Headers.Get("Authorization");
+                var scheme = HttpSigningConstants.AccessTokenParameterNames.AuthorizationHeaderScheme + " ";
+                if (authorizationHeader.StartsWith(scheme))
+                {
+                    var token = authorizationHeader.Substring(scheme.Length);
+                    return token;
+                }
+
+                return null;
+            }
+
+            if (ctx.Request.ContentType != null && 
+                ctx.Request.ContentType.StartsWith("application/x-www-form-urlencoded", StringComparison.OrdinalIgnoreCase))
+            {
+                var form = await ctx.Request.ReadFormAsync();
+                if (form != null)
+                {
+                    var token = form.Get(HttpSigningConstants.AccessTokenParameterNames.RequestParameterName);
+                    return token;
+                }
+
+                return null;
+            }
+
+            if (ctx.Request.Query != null)
+            {
+                var token = ctx.Request.Query.Get(HttpSigningConstants.AccessTokenParameterNames.RequestParameterName);
+                return token;
+            }
+
+            return null;
+        }
+
+        //public string GetAccessTokenFromPopToken(string token)
+        //{
+        //    var json = Jose.JWT.Payload(token);
+
+        //    var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+        //    if (values.ContainsKey(HttpSigningConstants.SignedObjectParameterNames.AccessToken))
+        //    {
+        //        return values[HttpSigningConstants.SignedObjectParameterNames.AccessToken] as string;
+        //    }
+
+        //    return null;
+        //}
+    }
+}
